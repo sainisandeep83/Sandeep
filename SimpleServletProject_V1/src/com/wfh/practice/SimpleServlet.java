c3aa5b03d680af4312b750b3c9ce3fab5532ad4d
jasan <jasan@192.168.0.106>
2020-10-28 10:07:12 -0400

    1.Change web.xml and added a new file html for apache default 2.First Simple servlet code

---
+++ SimpleServletProject_V1/src/com/wfh/practice/SimpleServlet.java
@@ -0,0 +1,31 @@
+package com.wfh.practice;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet(
+		description = "Simple Servlet V1", 
+		urlPatterns = { 
+				"/SimpleServletV1", 
+				"/SimpleServletV1-1"
+		})
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from Get Method");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
